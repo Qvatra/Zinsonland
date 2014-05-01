fd97da9eb8ac2e38dee53231c93ff016f5e1aae2
Qvatra <mail2zin@gmail.com>
2014-05-01 14:43:19 +0200

    missing scripts

---
+++ Assets/Scripts/Supply(Rifle).js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D (hitInfo : Collider2D) {
+	if (hitInfo.name == "player01") {
+		_GM.weapon = 'Assault_rifle';
+		Destroy(gameObject);
+	}
+}
