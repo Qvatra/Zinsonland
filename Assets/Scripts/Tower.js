f7ae5e3e7099cdea53a97d076f13f78d3d675971
Qvatra <mail2zin@gmail.com>
2014-06-16 00:50:26 +0200

    added turret

--- Assets/Scripts/Tower.js
+++ Assets/Scripts/Tower.js
@@ -1,9 +1,36 @@
 ﻿#pragma strict
 
-function Start () {
+public class Tower extends MonoBehaviour {
+protected var range: float;
+protected var rate: float;
+protected var damage: float;
+protected var shotVelocity: float;
+protected var rotationVelocity: int = 45; //grad per sec
+protected var scanTime: float = 1f;
+private var time: int;
+private var currentAng: float;
 
+function Awake(){
+	time = Time.time;
 }
 
-function Update () {
+public function Tower(){}
+    
+public function rotate(gun: GameObject, angle: float){
+	//if(Time.time - time > scanTime){
+ 		Debug.Log('rotating at scan time');
+ 		currentAng = gun.transform.eulerAngles.z;
 
+ 		if(currentAng < angle) {
+ 			gun.transform.Rotate(Vector3(0,0,1), rotationVelocity * Time.deltaTime);
+ 		}
+ 	//	time = Time.time;
+ 	//}
+}
+        
+public function fire(shotPrefab: GameObject){
+	Debug.Log('firing');
+}
+
+                 
 }
---
+++ Assets/Scripts/Turret.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var gunObj: GameObject;
+var tripodObj: GameObject;
+var gunAnim : Animator;
+var tripodAnim : Animator;
+var angle: float = 45f;
+
+public class Turret extends Tower {}
+
+function Awake(){
+	gameObject.AddComponent(Tower);
+		
+	gunAnim.speed = 0.5f;
+	gunAnim.SetBool("build", true);
+	//yield WaitForSeconds(1);
+	tripodAnim.speed = 0.5f;
+	tripodAnim.SetBool("build", true);
+}
+
+function Start () {
+	
+}
+
+function Update () {
+	rotate(gunObj, 60f);
+}
